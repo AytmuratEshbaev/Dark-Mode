da84de9d0a85352925723305cb465bf4dff1b09c
Aytmurat Eshbaev <a.eshbaev1@gmail.com>
2022-09-07 12:56:48 +0500

    Finish

--- src/index.js
+++ src/index.js
@@ -2,13 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
+class Card extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            darkMode: false
+        }
+        this.handleInput = this.handleInput.bind(this);
+    }
+    handleInput(event) {
+        this.setState({ darkMode: event.target.checked });
+    }
+    render() {
+        return (
+            <div className="cardWrapper">
+                <div className={"card" + (this.state.darkMode ? " dark" : " white")}>
+                    <h2 className="card__title">Render Props!</h2>
+                    <p className="card__text">I was build with a two class components. A wrapper that handles state and state changing functions and a child that uses that functionality.</p>
+                    <label htmlFor="darkMode">
+                        <input type="checkbox" id="darkMode" onInput={this.handleInput} />Dark Mode
+                    </label>
+                </div>
+            </div>
+        )
+    }
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Card />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
